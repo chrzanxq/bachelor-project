6e3946c14456d90500d74a2b0be68b903dea52c9
chrzanxq <marcinch199@gmail.com>
2023-06-03 14:12:17 +0200

    Login Page Progress

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,8 +1,80 @@
 'use client';
 
+import {useCallback, useState} from "react";
+import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
+import  Input from "../../components/inputs/Input";
+type Variant = "LOGIN" | "REGISTER";
+
 const AuthForm = () => {
+    const [variant, setVariant] = useState<Variant>("LOGIN");
+    const [isLoading, setIsLoading] = useState(false);
+    const toggleVariant = useCallback(() =>{
+        if (variant === 'LOGIN'){
+            setVariant('REGISTER')
+        } else {
+            setVariant('LOGIN')
+        }
+    }, [variant]);
+
+    const {
+        register,
+        handleSubmit,
+        formState:{
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        // placeholders
+        if(variant === 'REGISTER'){
+            // Axios Register
+        }
+
+        if(variant === 'LOGIN'){
+            // NextAuth Sing In
+        }
+
+        const socialAction = (action: string) => {
+            setIsLoading(true)
+
+            // NextAuth Social Sign In
+        }
+    }
     return (
-        <div>Auth Form!</div>
+        <div
+         className="
+           mt-8
+           sm:mx-auto
+           sm:w-full
+           sm:max-w-md   
+         "
+        >
+        <div
+         className="
+          bg-white
+          px-4
+          py-8
+          shadow
+          sm:rounded-lg
+          sm:px-10
+         "
+        >
+            <form
+             className="space-y-6"
+             onSubmit={handleSubmit(onSubmit)}
+            >
+                <Input label="Email"/>
+            </form>    
+        </div>
+        </div>
     );
 }
 
