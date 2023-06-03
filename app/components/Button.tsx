255ba2c35ab8b5c5413378d41ce770eb403c55ec
chrzanxq <marcinch199@gmail.com>
2023-06-03 17:59:45 +0200

    logo + styling change

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -28,20 +28,20 @@ const Button: React.FC<ButtonProps> = ({
         className={clsx(`
             flex
             justify-center
-            rounded-md
+            rounded-xl
             px-3
             py-2
-            text-sm
+            text-md
             font-semibold
             focus-visible:outline
-            focus-visible:outline-2
+            focus-visible:outline-3
             focus:visible:outline-offset-2
             `,
             disabled && "opacity-50 cursor-default",
             fullWidth && "w-full",
             secondary ? 'text-gray-900' : 'text-white',
             danger && "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
-            !secondary && !danger && "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
+            !secondary && !danger && "bg-green-500 hover:bg-green-600 focus-visible:outline-green-600"
 
             )}
     >
